bc50dbaafebbf9d5e430d56bde3b7037640506dc
Kalaser <41234308+Kalaser@users.noreply.github.com>
2025-08-17 13:31:10 +0800

    Update update_HelloGitHub_repos.py

--- update_HelloGitHub_repos.py
+++ update_HelloGitHub_repos.py
@@ -1,12 +1,7 @@
 import time
 import re
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
+
+from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 
 README_FILE = "README.md"
